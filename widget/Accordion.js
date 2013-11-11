ee621b625c989dc37f4167d3eece12d2603bfe22
Tim Roediger <superdweebie@gmail.com>
2013-11-12 08:21:08 +1000

    improve accordion to use css transitions

--- widget/Accordion.js
+++ widget/Accordion.js
@@ -1,36 +1,27 @@
 define([
     'dojo/_base/declare',
-    'dojo/_base/fx',
-    'dojo/fx',
-    'dojo/fx/easing',
     'dojo/_base/array',
     'dojo/_base/lang',
     'dojo/on',
     'dojo/string',
-    'dojo/query',
     'dojo/dom-attr',
     'dojo/dom-construct',
-    'dojo/dom-geometry',
     'dijit/a11yclick',
+    '../cssfx',
     './_WidgetBase',
     'dojo/text!./template/AccordionItem.html',
-    '../less!../vendor/bootstrap/less/accordion.less',
-    '../less!../vendor/bootstrap/less/component-animations.less'
+    '../less!../vendor/bootstrap/less/accordion.less'
 ],
 function (
     declare,
-    baseFx,
-    fx,
-    easing,
     array,
     lang,
     on,
     string,
-    query,
     domAttr,
     domConstruct,
-    domGeom,
     a11yclick,
+    cssfx,
     WidgetBase,
     itemTemplate
 ){
@@ -65,51 +56,30 @@ function (
 
                 domConstruct.place(string.substitute(this.itemTemplate, {title: title}), srcNode, 'after');
                 item = {domNode: srcNode.nextElementSibling};
-                query('[data-dojo-attach-point]', item.domNode).forEach(function(attachNode){
-                        item[domAttr.get(attachNode, 'data-dojo-attach-point')] = attachNode;
-                });
+                item.body = item.domNode.lastElementChild;
+                item.containerNode = item.body.firstElementChild;
                 domConstruct.place(srcNode, item.containerNode);
                 this.items.push(item);
 
-                on(item.toggle, a11yclick.click, lang.hitch(this, function(e){
+                on(item.domNode.firstElementChild, a11yclick.click, lang.hitch(this, function(e){
                     e.preventDefault();
                     this._toggleItem(item);
                 }))
             },
 
             _toggleItem: function(item){
-                var toggleFx = [];
-                array.forEach(this.items, lang.hitch(this, function(accItem){
-                    if (accItem.open){
-                        toggleFx.push(this._getToggleFx(accItem));
-                    }
-                }));
-                toggleFx.push(this._getToggleFx(item));
-                fx.combine(toggleFx).play();
-            },
-
-            _getToggleFx: function(item){
-                var height = domGeom.position(item.body).h,
-                    containerNodeHeight = domGeom.position(item.containerNode).h;
-
-                if (height > 0){
-                    item.open = false;
-                    return baseFx.animateProperty({
-                        node: item.body,
-                        properties: {
-                            height: 0
-                        },
-                        easing: easing.quartInOut
-                    });
+                if (domAttr.get(item.body, 'data-havok-open')){
+                    domAttr.set(item.body, 'data-havok-open', false);
+                    item.body.style['height'] = item.body.scrollHeight + 'px';
+                    setTimeout(function(){ //this brief delay is required to allow the style to switch from auto to an explicit number. Without the delay, the css transition may not fire.
+                        item.body.style['height'] = '0';
+                    }, 20);
                 } else {
-                    item.open = true;
-                    return baseFx.animateProperty({
-                        node: item.body,
-                        properties: {
-                            height: containerNodeHeight
-                        },
-                        easing: easing.quartInOut
-                    });
+                    domAttr.set(item.body, 'data-havok-open', true);
+                    item.body.style['height'] = item.body.scrollHeight + 'px';
+                    on.once(item.body, cssfx.transitionEndEvent(), lang.hitch(this, function(){
+                        item.body.style['height'] = 'auto';
+                    }));
                 }
             }
         }
