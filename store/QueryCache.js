db6cef29b056f36edd346e0cba16cae814534a82
Tim Roediger <superdweebie@gmail.com>
2014-02-03 15:07:01 +1000

    rewrite store wrappers

---
+++ store/QueryCache.js
@@ -0,0 +1,106 @@
+define([
+    'dojo/_base/lang',
+    'dojo/when',
+    'dojo/Deferred',
+    'dojo/store/util/QueryResults',
+    'dojo/store/Cache'
+],
+function(
+    lang,
+    when,
+    Deferred,
+    QueryResults,
+    Cache
+){
+
+    return function(masterStore, cachingStore, options){
+
+        options = options || {};
+
+        var queryCache = {},
+            innerStore = Cache(masterStore, cachingStore, options);
+
+        return lang.delegate(innerStore, {
+            queryCache: {},
+
+            clearQueryCacheMode: options.clearQueryCache || 'auto', //auto|manual
+
+            ttl: options.ttl || 1000 * 60 * 5, //default ttl five minutes
+
+            clearQueryCache: function(){
+                queryCache = {};
+            },
+
+            query: function(query, directives){
+                directives = directives || {};
+                var queryString = {},
+                    idOnly = true,
+                    idQuery,
+                    i;
+
+                for (i in query){
+                    if (typeof query[i] == 'function'){
+                        idOnly = false;
+                        queryString = undefined;
+                        break;
+                    } else if (query[i] instanceof RegExp){
+                        idOnly = false;
+                        queryString[i] = {'$regex': query[i].toString()}
+                    } else if (i == innerStore.idProperty){
+                        idQuery = true;
+                        queryString[i] = query[i];
+                    } else {
+                        idOnly = false;
+                        queryString[i] = query[i];
+                    }
+                }
+                if (queryString) queryString = JSON.stringify(queryString);
+
+                if (!queryString || !queryCache[queryString] || queryCache[queryString].expires < (new Date)){
+
+                    var expires = new Date,
+                        done = new Deferred;
+                    queryCache[queryString] = {
+                        expires: expires.setMilliseconds(expires.getMilliseconds() + directives.ttl ? directives.ttl : options.ttl),
+                        result: new QueryResults(done)
+                    };
+
+                    if (idOnly && idQuery){
+                        when(innerStore.get(query[innerStore.idProperty]), lang.hitch(this, function(data){
+                            done.resolve([data]);
+                        }))
+                    } else {
+                        var innerResult = innerStore.query(query, directives);
+                        when(innerResult, lang.hitch(this, function(data){
+                            done.resolve(data);
+                        }));
+                    }
+                }
+                return queryCache[queryString].result;
+            },
+
+            add: function(object, directives){
+                if (this.clearQueryCacheMode == 'auto') this.clearQueryCache()
+                return innerStore.add(object, directives);
+            },
+
+            put: function(object, directives){
+                if (this.clearQueryCacheMode == 'auto') this.clearQueryCache()
+                return innerStore.put(object, directives);
+            },
+
+            remove: function(id, directives){
+                if (this.clearQueryCacheMode == 'auto') this.clearQueryCache()
+                return innerStore.remove(id, directives);
+            },
+
+            evict: function(id){
+                if (this.clearQueryCacheMode == 'auto') this.clearQueryCache()
+                return innerStore.evict(id);
+            }
+        })
+    }
+})
+
+
+
---
+++ store/Throttle.js
@@ -0,0 +1,125 @@
+define([
+    'dojo/_base/lang',
+    'dojo/Deferred',
+    'dojo/store/util/QueryResults',
+    'dojo/when'
+],
+function (
+    lang,
+    Deferred,
+    QueryResults,
+    when
+){
+
+    return function(store, throttle){
+
+        // throttle: Int
+        //      A value in milliseconds
+        //      The store will not be accessed at a rate faster than the set throttle interval.
+        var queue = [],
+            timeout,
+            process = function(){
+                if (!timeout){
+                    timeout = setTimeout(function(){
+                        delete(timeout);
+                        process();
+                    }, throttle)
+
+                    var request = queue.shift();
+                    if (request.type == 'query'){
+                        when(store.query(request.query, request.directives), function(data){
+                            request.done.resolve(data);
+                        })
+                    } else if (request.type == 'get'){
+                        when(store.get(request.id), function(data){
+                            request.done.resolve(data);
+                        })
+                    } else if (request.type == 'put'){
+                        when(store.put(request.object, request.directives), function(){
+                            request.done.resolve();
+                        })
+                    } else if (request.type == 'add'){
+                        when(store.add(request.object, request.directives), function(){
+                            request.done.resolve();
+                        })
+                    } else if (request.type == 'remove'){
+                        when(store.remove(request.id), function(){
+                            request.done.resolve();
+                        })
+                    }
+                }
+            };
+
+        lang.delegate(store, {
+
+            query: function(query, directives){
+                var done = new Deferred,
+                    result = new QueryResults(done),
+                    request = {
+                        type: 'query',
+                        query: query,
+                        directives: directives,
+                        done: done
+                    }
+
+                queue.push(request);
+                process();
+                return result;
+            },
+
+            get: function(id){
+                var done = new Deferred,
+                    request = {
+                        type: 'get',
+                        id: id,
+                        done: done
+                    }
+
+                queue.push(request);
+                process();
+                return done;
+            },
+
+            put: function(object, directives){
+                var done = new Deferred,
+                    request = {
+                        type: 'put',
+                        object: object,
+                        directives: directives,
+                        done: done
+                    }
+
+                queue.push(request);
+                process();
+                return done;
+            },
+
+            add: function(object, directives){
+                var done = new Deferred,
+                    request = {
+                        type: 'add',
+                        object: object,
+                        directives: directives,
+                        done: done
+                    }
+
+                queue.push(request);
+                process();
+                return done;
+            },
+
+            remove: function(id){
+                var done = new Deferred,
+                    request = {
+                        type: 'remove',
+                        id: id,
+                        done: done
+                    }
+
+                queue.push(request);
+                process();
+                return done;
+            }
+        })
+    }
+});
